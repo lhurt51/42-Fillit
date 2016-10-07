2c967934b0f5a212d980c1b977b510e97697f3fc
Liam Hurt <lhurt@e1z1r5p40.42.us.org>
2016-10-07 14:30:23 -0700

    My solve is almost finnished just need to condense and test

--- fillit.h
+++ fillit.h
@@ -29,14 +29,8 @@ typedef struct		s_list
 	struct s_list	*next;
 }					t_list;
 
-void				ft_putchar(char c);
-void				ft_putchar_fd(char c, int fd);
-void				ft_putstr(char const *str);
-void				ft_putstr_fd(char const *s, int fd);
 void				ft_putendl(char const *s);
-void				ft_putendl_fd(char const *s, int fd);
 void				ft_putnbr(int nb);
-void				ft_putnbr_fd(int n, int fd);
 void				*ft_memset(void *b, int c, size_t len);
 void				ft_bzero(void *s, size_t n);
 int					ft_isalpha(int c);
