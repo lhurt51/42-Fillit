c5573420e430ee703f39b9a942af803aab029a65
Liam Hurt <lhurt@e1z1r5p40.42.us.org>
2016-10-13 12:10:28 -0700

    Was an include that wasnt nessicary

--- store.c
+++ store.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "fillit.h"
 
 void	findkey(int *x, int *y, char **grid)
